4b2dc31287a9bb9e791a33f4d5a453f96777abaf
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-05-19 15:51:18 +0600

    responsive for small device

---
+++ Javascript/index.js
@@ -0,0 +1,15 @@
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const target = event.target;
+    const fullName = target.fullname.value;
+    const email = target.email.value;
+    const subject = target.subject.value;
+
+    const Values = fullName && email && subject;
+
+    if (!Values) {
+        document.getElementById("errorText").innerText = "* Marked fields are required to fill."
+    } else {
+        alert("Message Sent")
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,40 @@ module.exports = {
   theme: {
     extend: {
       screens:{
-        "lg":"1220px"
+        'sm': '576px',
+        'md': '768px',
+        'lg': '1220px',
+      
       },
       colors:{
 Primary:"#999999",
-Green:"#28e98c"
+Green:"#28e98c",
+Secondary:"#656565"
       },
       borderColor:{
         Primary:"#565656",
-        Secondary:"#999999"
       },
       fontFamily:{
 Inter:"Inter, sans-serif"
       }
     },
   },
- 
-  plugins: [
+   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     require("daisyui")
   ],
 }
+
+// <576px
+// Small	sm	≥576px
+// Medium	md	≥768px
+// Large	lg	≥992px
+// Extra large	xl	≥1200px
+// Extra extra large	xxl	≥1400px
+
+// 640px	@media (min-width: 640px) { ... }
+// md	768px	@media (min-width: 768px) { ... }
+// lg	1024px	@media (min-width: 1024px) { ... }
+// xl	1280px	@media (min-width: 1280px) { ... }
+// 2xl	1536px
