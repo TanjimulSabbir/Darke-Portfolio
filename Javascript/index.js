7ac04c3142d3678e2c4086358ec8cb0c51755eb6
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-05-20 19:41:49 +0600

    left header done,alh

--- Javascript/index.js
+++ Javascript/index.js
@@ -17,22 +17,65 @@ const handleSubmit = (event) => {
 
 // Right Header Menu Function
 let OpenMenu = false;
-const handleRightHeaderMenu = (e) => {
-    OpenMenu = !OpenMenu;
-    if (OpenMenu) {
-       document.getElementById("RightMenuContainer").style.display="block"
-    }
-    const RightMenuContainer = document.getElementById("RightMenuContainer");
-    const NewDiv = document.createElement("div");
-    NewDiv.innerHTML = ``
-    console.log(RightMenuContainer)
-    RightMenuContainer.appendChild(NewDiv);
-}
 
-const handleClose = () => {
-    OpenMenu = false;
-    if (!OpenMenu) {
-        document.getElementById("DrawerClose").click();
-        document.getElementById("RightMenuContainer").style.display="none"
-    }
-}
+// const handleOpen = () =>{
+//     OpenMenu = !OpenMenu;
+//     window.onload = function () {
+//         if (OpenMenu) {
+//             document.getElementById("openDrawer").click();
+//           return  document.getElementById("RightMenuContainer").style.display = "block";
+//         } 
+       
+//     };
+//     alert(OpenMenu + "handleOpenBtn")
+// }
+
+// const handleRightHeaderMenu = () => {
+//     if (OpenMenu) {
+//         OpenMenu = false;
+//          document.getElementById("RightMenuContainer").style.display = "none";
+//     } 
+ 
+//     alert(OpenMenu)
+// }
+
+
+
+console.log(document.getElementById("RightMenuContainer"))
+
+//     OpenMenu = !OpenMenu;
+//     if (!OpenMenu) {
+//         window.onload=function(){
+//             document.getElementById("openDrawer").click();
+//           };
+//         return document.getElementById("RightMenuContainer").style.display="none";
+//     } else {
+//         document.getElementById("RightMenuContainer").style.display="block"
+//     document.getElementById("RightMenuContainer").innerHTML=`<div class="drawer drawer-end border border-red-600">
+//     <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+//     <div class="drawer-content ">
+//     </div>
+//     <div class="drawer-side">
+//       <label for="my-drawer-4" id="DrawerClose" class="drawer-overlay"></label>
+//       <ul class="menu p-4 w-full max-w-[345px] bg-black z-50 text-base-content">
+//         <li><a>Sidebar Item 1</a></li>
+//         <li><a>Sidebar Item 2</a></li>
+//       </ul>
+//     </div>
+//   </div>`
+// }
+// style.display = "block"
+// const container=document.getElementById("RightMenuContainer");
+//     window.onload = function () {
+//                 document.getElementById("openDrawer").click();
+//     };
+//     console.log(container)
+// }
+
+// const handleClose = (event) => {
+//     window.onload = function () {
+//         document.getElementById("RightMenuContainer").style.display = "block";
+//         document.getElementById("DrawerClose").click();
+//     };
+//     document.getElementById("RightMenuContainer").style.display = "none";
+// }
